6d5596000fa2fb0f95a009f7fc1f10a56fb881c3
Jonathan Kim <jonathankim86@gmail.com>
2021-03-13 17:36:34 -0300

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  render(){
+  // "render()" va a crear un componente.
+    return(
+      <> {/*React fragment*/}
+        <header>
+          <h1>Data Warehouse</h1>
+        </header>
+        <Nav/>
+        <Nav/>
+        <Nav/>
+      </>
+    )}}
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Nav extends React.Component{
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li><a href="">link1</a></li>
+                    <li><a href="">link2</a></li>
+                    <li><a href="">link3</a></li>
+                </ul>
+            </nav>
+        )}}
+
+export default Nav;
