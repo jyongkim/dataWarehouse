9193166007e1e19cbf2f98aff231bca6c172f83b
Jonathan Kim <jonathankim86@gmail.com>
2021-04-01 15:11:54 -0300

    Modelo de roles de usuario

---
+++ src/models/role.js
@@ -0,0 +1,25 @@
+let dbConn = ''
+
+let Role = function(role) {
+    this.role = role.role
+}
+
+Role.create = (role, result) => {
+    dbConn.query('INSERT INTO Roles SET ?', role, (err, res) => {
+        err ? result(err, null) : result(null, res)
+})}
+
+Roles.read = (result) => {
+    dbConn.query('SELECT * FROM Roles', (err, res) => {
+        err ? result(err, null) : result(null, res)
+})}
+
+Roles.update = (id, role, result) => {
+    dbConn.query('UPDATE Roles SET ? WHERE id_role = ?', [role, id], (err, res) => {
+        err ? result(err, null) : result(null, res)
+})}
+
+Roles.delete = (id, result) => {
+    dbConn.query('DELETE FROM Roles WHERE id_role = ?', id, (err, res) => {
+        err ? result(err, null) : result(null, res)
+})}
