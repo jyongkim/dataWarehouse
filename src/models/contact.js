fa837f11faee50631666f132d2a5deb40dc4afb2
Jonathan Kim <jonathankim86@gmail.com>
2021-04-01 14:58:10 -0300

    Modelo de contactos por compañía

--- src/models/contact.js
+++ src/models/contact.js
@@ -3,6 +3,8 @@ let dbConn = '';
 let Contact = function(contact) {
     this.first_name = contact.first_name
     this.last_name = contact.last_name
+    this.doc_type = contact.doc_type
+    this.doc_num = contact.doc_num
     this.position = contact.position
     this.interest = contact.interest
     this.id_city = contact.id_city
@@ -18,17 +20,17 @@ Contact.read = (id, result) => {
         err ? result(err, null) : result(null, res)
 })}
 
-Contact.find = (id, result) => {
-    dbConn.query('SELECT * FROM Contacts WHERE id_company = ? AND (user_name LIKE ? OR last_name LIKE ?)', id, (err, res) => {
+Contact.find = (id, contact, result) => {
+    dbConn.query('SELECT * FROM Contacts WHERE id_company = ? AND doc_num = ? AND id_city = ?', [id, contact.doc_num, contact.id_city], (err, res) => {
         err ? result(err, null) : result(null, res)
 })}
 
 Contact.update = (id, user, result) => {
-    dbConn.query('UPDATE Contacts SET ? WHERE id_user = ?', [user, id], (err, res) => {
+    dbConn.query('UPDATE Contacts SET ? WHERE id_contact = ?', [user, id], (err, res) => {
         err ? result(err, null) : result(null, res)
 })}
 
 Contact.delete = (id, result) => {
-    dbConn.query('DELETE FROM Contacts WHERE id_user = ?', id, (err, res) => {
+    dbConn.query('DELETE FROM Contacts WHERE id_contact = ?', id, (err, res) => {
         err ? result(err, null) : result(null, res)
 })}
