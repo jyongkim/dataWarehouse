95151711b4eea2cc0776e65a98305a211759dcbf
Jonathan Kim <jonathankim86@gmail.com>
2021-04-04 21:28:55 -0300

    Correcciones ortográficas menores

--- src/models/user.js
+++ src/models/user.js
@@ -29,18 +29,18 @@ User.update = (id, user, result) => {
 })}
 
 User.delete = (id, result) => {
-    // elimina las preferencias consultando contactos y consultando compañias
+    // Elimina las preferencias consultando contactos y consultando compañías.
     dbConn.query(`DELETE cp FROM contact_preferences AS cp
     JOIN contacts AS con ON con.id_contact = cp.id_contact
     JOIN companies AS com ON com.id_company = con.id_company
     WHERE id_user = ?`, id)
-    // elimina los contactos consultando las compañias
+    // Elimina los contactos consultando las compañías.
     dbConn.query(`DELETE con FROM contacts AS con
     JOIN companies AS com ON con.id_company = com.id_company
     WHERE id_user = ?`, id)
-    // elimina las compañias utilizando como referente el id_user
+    // Elimina las compañías utilizando como referente al id_user.
     dbConn.query('DELETE FROM Companies WHERE id_user = ?', id)
-    // una vez eliminado todo, elimina el usuario y realiza el callback
+    // Una vez eliminado todo, elimina el usuario y realiza el callback.
     dbConn.query('DELETE FROM Users WHERE id_user = ?', id, (err, res) => {
         err ? result(err, null) : result(null, res)
 })}
