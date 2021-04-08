9b83f0673b2613301dc7aa80b93e6fc5ea513cf4
Jonathan Kim <jonathankim86@gmail.com>
2021-04-08 17:47:30 -0300

    Modelo de compañía

---
+++ src/routes/company.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const companyController = require('../controllers/company');
+    // El ID hace referencia al id_user.
+    router.post('/:id', companyController.create)
+    router.get('/:id', companyController.read)
+    // El ID hace referencia al id_company.
+    router.put('/:id', companyController.update)
+    router.delete('/:id', companyController.delete)
+
+module.exports = router;
