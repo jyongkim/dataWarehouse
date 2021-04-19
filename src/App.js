40e2df21195c5707fa536db92623d3fecdc64de8
Jonathan Kim <jonathankim86@gmail.com>
2021-04-19 16:26:20 -0300

    Estilos visuales

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { Component } from 'react';
 import Nav from './components/Nav';
-import Login from './components/Login'
 import Form from './components/Form'
 import Table from './components/Table';
+import formFields from './components/formField.json'
 
 class App extends Component{
   constructor(props){
@@ -11,7 +11,8 @@ class App extends Component{
       this.state = {
         links: ['Contactos', 'Compañías', 'Usuarios','Región/Ciudad'],
         userId: 1,
-        userData: []
+        userData: [],
+        formFields: formFields.Login
   }}
   getData = (url) => {
     let server = `http://localhost:3200/`
@@ -19,20 +20,36 @@ class App extends Component{
       .then(response => response.json())
       .then(data => {this.setState({userData: data})})
   }
+
   render(){
   // "render()" va a crear un componente.
     return(
       <> {/*React fragment*/}
-        <header>
-          <h1>Data Warehouse</h1>
-        </header>
-        <Login />
         <Nav links = {this.state.links}/> {/*Props*/}
-      <button onClick = {() => this.getData(`user/${this.state.userId}`)}>Usuarios</button>
-      <button onClick = { () => this.getData(`company/${this.state.userId}`)}> Compañías</button>
-      <button onClick = {() => {this.getData(`contact/${this.state.userId}`)}}>Contactos</button>
+      <button onClick = {() =>{ 
+        this.getData(`user/${this.state.userId}`)
+        this.setState({formFields: formFields.User})
+      }}>Usuarios</button>
+      <button onClick = { () =>{ 
+        this.getData(`company/${this.state.userId}`)
+        this.setState({formFields: formFields.Company})
+      }}> Compañías</button>
+      <button onClick = {() =>{ 
+        this.getData(`contact/${this.state.userId}`)
+        this.setState({formFields: (formFields.Contact + formFields.Preference)})
+      }}>Contactos</button>
+      <Form data={this.state.formFields}/>
       <Table data={this.state.userData}/>
       </>
+    )}
+  componentDidUpdate(){
+    return(
+      console.log(this.state.formFields)
+    )
+  }
+  componentDidMount(){
+    return(
+      console.log(formFields)
     )}}
 
 export default App;
