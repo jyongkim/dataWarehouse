3d8c57d691d8590290b2a9ad62da43341ff63484
Jonathan Kim <jonathankim86@gmail.com>
2021-11-03 23:25:39 -0300

    Se continúa trabajando en el ABM de Regiones

--- src/Backend/model/region.js
+++ src/Backend/model/region.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 
 // Option 2: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('Data_Warehouse', 'root', null, {
-    host: 'localhost',
+const sequelize = new Sequelize('Data_Warehouse', 'root', 'root', {
+    host: '127.0.0.1',
     dialect: 'mysql'
 });
 try {
